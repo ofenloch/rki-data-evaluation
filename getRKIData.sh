54a33ca60c8bbf063cf36beb484f9dfd7aa6dd15
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2021-04-05 19:57:51 +0200

    download DIVI data, too

--- getRKIData.sh
+++ getRKIData.sh
@@ -65,6 +65,11 @@ echo "NOW             ${NOW}"
 # Excel-Datei mit Altersverteilung
 /usr/bin/wget 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Altersverteilung.xlsx;jsessionid=BBA0E867314E7133E733D3E5D3459A48.internet082?__blob=publicationFile' --append-output ${RKI_DATA_DIR}/wget-rki.log --output-document ${RKI_DATA_DIR}/RKI-Altersverteilung.xlsx
 
+
+# Die Zeitreihen des DIVI-Intensivregisters gibt es hier
+/usr/bin/wget https://diviexchange.blob.core.windows.net/%24web/bundesland-zeitreihe.csv --append-output ./rki-data/wget-rki.log --output-document ./rki-data/bundesland-zeitreihe.csv
+
+
 cd ${THIS_DIR}
 
 /usr/bin/git commit ${RKI_DATA_DIR}/*.xlsx -m"${0}: add data automatically downloaded at ${NOW}"
