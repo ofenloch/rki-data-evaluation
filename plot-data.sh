8e231d979dfb7ab682a95651bf9ce031f13aa150
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2021-04-12 19:33:55 +0200

    make the formatting of test data a function

--- plot-data.sh
+++ plot-data.sh
@@ -63,62 +63,62 @@ function iso_week_num_to_date() {
     fi
     #echo "   first_mon_in_year is ${first_mon_in_year}   ( $(/usr/bin/date -d ${first_mon_in_year} +%a) )"
     echo "${mon};${sun}"
-}
+} # function iso_week_num_to_date() {
 
-# delete the temporary file ./data-tests.csv.tmp:
-/bin/rm -f ./data-tests.csv.tmp
-# initialize the line counter:
-n=1
-# Set semicolon as the delimiter:
-delimiter=";"
-# read the file line by line and process eacj line:
-while read -r line; do 
-    #echo "line ${n} is \"${line}\""
-    echo -n "processing line ${n}:"
-    n=$((n+1))
-    # replace '*' e.g. "11/2021*;" by "11/2021;"
-    line=${line/\*;/;}
-    # split the line at the delimiter
-    s=${line}${delimiter}
-    #echo "  s is \"${s}\""
-    array=();
-    while [[ $s ]]; do
-        array+=( "${s%%"$delimiter"*}" );
-        s=${s#*"$delimiter"};
-    done;
-    #echo "array[0] is \"${array[0]}\""
-    first=${array[0]}
-    weeknr=1
-    year=2022
-    if [[ ${#first} == 7 ]] ; then
-        # we have something like "43/2020"
-        weeknr=${first:0:2}
-        year=${first:3:5}
-    fi
-    if [[ ${#first} == 6 ]] ; then
-        # we have something like "3/2021"
-        weeknr=${first:0:1}
-        year=${first:2:5}
-    fi
-    echo "  weeknr = ${weeknr} ; year = ${year}"
-    firstnew=$(iso_week_num_to_date ${weeknr} ${year})
-    if [[ ${#first} > 5 ]] ; then
-        # we should have a valid  week number and a valid date range
-        echo -n "${firstnew};" >> ./data-tests.csv.tmp
-    else
-        # no valid week number
-        echo -n ";;" >> ./data-tests.csv.tmp
-    fi
-    # simply append the original line
-    echo "${line}" >> ./data-tests.csv.tmp
-    # declare -p array
-done < ./rki-data/RKI-Testzahlen-gesamt-csv/1_Testzahlerfassung.csv
+function format_test_data() {
+    # delete the temporary file ./data-tests.csv.tmp:
+    /bin/rm -f ./data-tests.csv.tmp
+    # initialize the line counter:
+    n=1
+    # Set semicolon as the delimiter:
+    delimiter=";"
+    # read the file line by line and process eacj line:
+    while read -r line; do 
+        #echo "line ${n} is \"${line}\""
+        echo -n "processing line ${n}:"
+        n=$((n+1))
+        # replace '*' e.g. "11/2021*;" by "11/2021;"
+        line=${line/\*;/;}
+        # split the line at the delimiter
+        s=${line}${delimiter}
+        #echo "  s is \"${s}\""
+        array=();
+        while [[ $s ]]; do
+            array+=( "${s%%"$delimiter"*}" );
+            s=${s#*"$delimiter"};
+        done;
+        #echo "array[0] is \"${array[0]}\""
+        first=${array[0]}
+        weeknr=1
+        year=2022
+        if [[ ${#first} == 7 ]] ; then
+            # we have something like "43/2020"
+            weeknr=${first:0:2}
+            year=${first:3:5}
+        fi
+        if [[ ${#first} == 6 ]] ; then
+            # we have something like "3/2021"
+            weeknr=${first:0:1}
+            year=${first:2:5}
+        fi
+        echo "  weeknr = ${weeknr} ; year = ${year}"
+        firstnew=$(iso_week_num_to_date ${weeknr} ${year})
+        if [[ ${#first} > 5 ]] ; then
+            # we should have a valid  week number and a valid date range
+            echo -n "${firstnew};" >> ./data-tests.csv.tmp
+        else
+            # no valid week number
+            echo -n ";;" >> ./data-tests.csv.tmp
+        fi
+        # simply append the original line
+        echo "${line}" >> ./data-tests.csv.tmp
+        # declare -p array
+    done < ./rki-data/RKI-Testzahlen-gesamt-csv/1_Testzahlerfassung.csv
+    sed -f ./sed-tests ./data-tests.csv.tmp > ./data-tests.csv
+    /bin/rm -f ./data-tests.csv.tmp
+} # function format_test_data() {
 
-sed -f ./sed-tests ./data-tests.csv.tmp > ./data-tests.csv
-/bin/rm -f ./data-tests.csv.tmp
-
-
-
+format_test_data
 
 # The DIVI data comes in one file for all German states. So, we split it up:
 for f in DEUTSCHLAND HAMBURG THUERINGEN SCHLESWIG_HOLSTEIN SACHSEN BADEN_WUERTTEMBERG SACHSEN_ANHALT BAYERN BERLIN MECKLENBURG_VORPOMMERN BREMEN NIEDERSACHSEN RHEINLAND_PFALZ SAARLAND NORDRHEIN_WESTFALEN HESSEN BRANDENBURG; do
